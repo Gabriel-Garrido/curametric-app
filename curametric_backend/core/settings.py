6a993015a0b8dab42fb31b62ec7901ea9f8fe103
Gabriel Garrido <gabrielgarrido89@gmail.com>
2025-02-21 00:59:17 -0300

    change debug 2

--- curametric_backend/core/settings.py
+++ curametric_backend/core/settings.py
@@ -15,8 +15,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS_DEV')
-
 # Application definition
 
 INSTALLED_APPS = [
