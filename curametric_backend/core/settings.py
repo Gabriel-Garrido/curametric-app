6b01d6691d514586d2f8601d2d4f1ff002f84bc9
Gabriel Garrido <gabrielgarrido89@gmail.com>
2025-02-21 01:56:10 -0300

    change order of whitenoise in settings.py

--- curametric_backend/core/settings.py
+++ curametric_backend/core/settings.py
@@ -53,9 +53,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
